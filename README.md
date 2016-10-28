07d64ac6ca5119f0c100b033272a84fc8075a4b4
yewei <yewei_andy@163.com>
2016-10-28 11:26:35 +0800

    新文件：   lesson1/mission1/Readme.md 	新文件：   lesson1/mission1/mission1.ino 	新文件：   lesson1/mission2/Readme.md 	新文件：   lesson1/mission2/mission2.ino

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # arduino_lesson_code
 广工电协开源硬件课程程序代码
+2016-10-28 更新第一课的源代码 yewei_andy
---
+++ lesson1/mission2/mission2.ino
@@ -0,0 +1,15 @@
+//频率变化的led,随机频率
+int freq = 0;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  freq = random(100,1000);
+  digitalWrite(13,1);
+  delay(freq);
+  digitalWrite(13,0);
+  delay(freq);
+}
