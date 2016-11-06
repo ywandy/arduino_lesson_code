55b5a4a7f183d08f8e436deafeef201b872f2e7f
yewei <yewei_andy@163.com>
2016-11-06 10:53:04 +0800

    上传串口的使用   lesson2/serial_simple_example/serial/serial.ino

---
+++ lesson2/serial_simple_example/serial/serial.ino
@@ -0,0 +1,12 @@
+//串口的使用
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600); //初始化串口，并设定波特率为9600
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("hello world!");//输出一行hello world
+  delay(1000); //延时1秒
+}
