ab54f2c1d52159545a5d5ef652cf28f6e3d81d11
yewei <yewei_andy@163.com>
2016-11-06 11:06:04 +0800

    修改：     lesson2/serial_simple_example/serial/serial.ino

--- lesson2/serial_simple_example/serial/serial.ino
+++ lesson2/serial_simple_example/serial/serial.ino
@@ -6,7 +6,8 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("hello world!");//输出一行hello world
+  Serial.println("hello world 1st");//输出一行hello world采用Arduino自带的换行
+  Serial.print("hello world 2nd \n");//输出一行hello world带标准C的换行
+  Serial.print("hello world 3rd");//输出一行hello world不带换行符
   delay(1000); //延时1秒
 }
